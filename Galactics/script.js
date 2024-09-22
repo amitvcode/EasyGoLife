1cf16ee029bc37922411b18e2d049f44d97ad188
Amit Vishwakarama <133197471+amitvcode@users.noreply.github.com>
2024-09-22 20:51:28 +0530

    Galactics

---
+++ Galactics/script.js
@@ -0,0 +1,15 @@
+
+// Menu Toggler 
+const Menutoggler = () => {
+  const toggleMenu = document.querySelector('.toggle');
+  const section = document.querySelector('.hero-section');
+  toggleMenu.classList.toggle('active');
+  section.classList.toggle('active');
+
+}
+
+// vedio speed control
+
+
+
+
